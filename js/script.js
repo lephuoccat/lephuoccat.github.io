2d1b39fd425b6b8ee51d5b257c4375c52c8b3f78
Cat Le <cat.le@duke.edu>
2025-10-26 00:48:08 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+// Get the button
+const backToTopButton = document.getElementById("back-to-top");
+
+// Show button when scrolling down 300px
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopButton.style.display = "flex";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
+
+// Smooth scroll to top when clicked
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
